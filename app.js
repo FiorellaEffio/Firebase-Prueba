685e4a0098126c6c351abbe38823ffc25ba7c276
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-07-10 09:24:13 -0500

    login con google

---
+++ app.js
@@ -0,0 +1,10 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+
+$('#login').click(function(){
+  firebase.auth()
+    .signInWithPopup(provider)
+    .then(function(result) {
+    console.log(result);
+    });
+
+});
